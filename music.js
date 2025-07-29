4ca94296820c12f074e806d4db5761abe071be12
Javier Jacobo <jjacobo1912@gmail.com>
2025-07-29 16:51:22 -0700

    Added new music, divs and counter for songs

--- music.js
+++ music.js
@@ -1,19 +1,25 @@
 const playlist = [
   "music/playlist/Birthday Boy.mp3",
-  "music/playlist/Rilkean Heart.mp3", 
-  "music/playlist/Everywhere.mp3",
   "music/playlist/Off Your Face.mp3",
+  "music/playlist/Roseblood.mp3",
+  "music/playlist/Rilkean Heart.mp3",
+  "music/playlist/Sarah.mp3",
+  "music/playlist/Born Slippy.mp3",
   "music/playlist/I Remember.mp3",
   "music/playlist/Movinin on you.mp3",
+  "music/playlist/A Certain Someone.mp3",
+  "music/playlist/The Last Hour.mp3",
   "music/playlist/Little Spacey.mp3",
   "music/playlist/Age of Consent.mp3",
   "music/playlist/Sometimes.mp3",
-  "music/playlist/Heres Where The Story Ends.mp3",
+  "music/playlist/Heres Where the Story Ends.mp3",
+  "music/playlist/Everywhere.mp3",
+  "music/playlist/Spinning.mp3",
   "music/playlist/Nothing Sweet.mp3",
   "music/playlist/Your Selfish Ways.mp3",
   "music/playlist/Tishbite.mp3",
-  "music/playlist/Suzanne.mp3",
-  "music/playlist/Drive It All Over Me.mp3"
+  "music/playlist/Drive It All Over Me.mp3",
+  "music/playlist/Suzanne.mp3"
 ];
 
 var currentSong = 0;
@@ -26,6 +32,15 @@ function playMusic(){
 
 function pauseMusic(){
     music.pause();
+}
+
+function muteMusic(){
+    if(music.muted){
+        music.muted = false;
+    }
+    else{
+        music.muted = true;
+    }
 }
 
 function next_song(){
@@ -54,6 +69,11 @@ function prev_song(){
     displaySong();
 }
 function displaySong() {
+    document.getElementById("song-num").textContent = (currentSong + 1) + "/" + playlist.length
     document.getElementById("current-song-display").textContent = playlist[currentSong];
 
+}
+
+window.onload = function(){
+    displaySong();
 }
